1c4aa4abc49e74e7ebd980ab957206c093c62e99
Grooteras <torsten.chacha@gmail.com>
2022-11-15 10:32:43 +0100

    removed uselessness

--- lommeregner/MainWindow.xaml.cs
+++ lommeregner/MainWindow.xaml.cs
@@ -15,13 +15,6 @@ using System.Windows.Shapes;
 
 namespace lommeregner
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-
-
-
     public partial class MainWindow : Window
     {
         public MainWindow()
