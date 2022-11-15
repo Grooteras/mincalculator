94100724e7b76772132e69c7f2a8c1d24061aef0
Grooteras <torsten.chacha@gmail.com>
2022-11-15 11:50:34 +0100

    added sign

--- lommeregner/MainWindow.xaml.cs
+++ lommeregner/MainWindow.xaml.cs
@@ -68,6 +68,7 @@ namespace lommeregner
                     break;
             }
                 resultatBox.Text = resultat + "";
+            sign.Text = operatorString;
             {
             }
         }
